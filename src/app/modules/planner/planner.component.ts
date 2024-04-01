85c1f5605c1272a2fc8ed342a3cc15c53fe7c5a1
Andrei Tatu <andrei.tatu.e@thalesdigital.io>
2024-04-01 17:01:08 +0300

    Implement planner page - WIP

--- src/app/modules/planner/planner.component.ts
+++ src/app/modules/planner/planner.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+const ELEMENT_DATA = [
+  {id: 1, name: 'name1', x: 1, y: 1},
+  {id: 1, name: 'name2', x: 1, y: 1},
+  {id: 1, name: 'name3', x: 1, y: 1},
+  {id: 1, name: 'name4', x: 1, y: 1},
+];
+
 
 @Component({
   selector: 'app-planner',
@@ -6,5 +15,17 @@ import { Component } from '@angular/core';
   styleUrl: './planner.component.css'
 })
 export class PlannerComponent {
+  displayedColumns: string[] = ['id', 'name', 'x', 'y'];
+  dataSource = ELEMENT_DATA;
+  pointForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    x: new FormControl('', Validators.required),
+    y: new FormControl('', Validators.required)
+  })
 
+  onAdd = () => {
+    console.log(this.pointForm.controls.name.value);
+    console.log(this.pointForm.controls.x.value);
+    console.log(this.pointForm.controls.y.value);
+  }
 }
--- src/app/modules/planner/planner.module.ts
+++ src/app/modules/planner/planner.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatCardModule } from '@angular/material/card';
 import { PlannerRoutingModule } from './planner-routing.module';
 import { PlannerComponent } from './planner.component';
-
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,7 +15,13 @@ import { PlannerComponent } from './planner.component';
   ],
   imports: [
     CommonModule,
-    PlannerRoutingModule
+    PlannerRoutingModule,
+    MatCardModule,
+    MatTableModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ]
 })
 export class PlannerModule { }
